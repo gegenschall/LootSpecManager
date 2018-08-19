d73257cb0680f9f669e91152dcccd57f799c1ca8
Req <lrkulak@gmail.com>
2018-08-19 14:32:57 -0700

    begin m+ settings and trailing whitespace trim
    mostly just whitespace trim, because apparently VSC doesn't enable that by default

--- core.lua
+++ core.lua
@@ -35,6 +35,7 @@ instances = {
 ]]
 local instances = {}
 (function(...)
+	--there isn't much transformation happening here, but the instance indices aren't hardcoded so new instances are easier to add
 	for _, v in ipairs({...}) do
 		tinsert(instances, {
 			name = v[1],
@@ -43,76 +44,97 @@ local instances = {}
 	end
 end)(
 	{"Uldir", {
-		[1] = {name = "Taloc", id = 2144}, 
-		[2] = {name = "MOTHER", id = 2141}, 
-		[3] = {name = "Fetid Devourer", id = 2128}, 
-		[4] = {name = "Zek'voz, Herald of N'zoth", id = 2136}, 
-		[5] = {name = "Vectis", id = 2134}, 
-		[6] = {name = "Zul, Reborn", id = 2145}, 
-		[7] = {name = "Mythrax the Unraveler", id = 2135}, 
+		[1] = {name = "Taloc", id = 2144},
+		[2] = {name = "MOTHER", id = 2141},
+		[3] = {name = "Fetid Devourer", id = 2128},
+		[4] = {name = "Zek'voz, Herald of N'zoth", id = 2136},
+		[5] = {name = "Vectis", id = 2134},
+		[6] = {name = "Zul, Reborn", id = 2145},
+		[7] = {name = "Mythrax the Unraveler", id = 2135},
 		[8] = {name = "G'huun", id = 2122}
 	}},
 	{"Atal'Dazar", {
-		[1] = {name = "Priestess Alun'za", id = 2084}, 
-		[2] = {name = "Vol'kaal", id = 2085}, 
-		[3] = {name = "Rezan", id = 2086}, 
+		[1] = {name = "Priestess Alun'za", id = 2084},
+		[2] = {name = "Vol'kaal", id = 2085},
+		[3] = {name = "Rezan", id = 2086},
 		[4] = {name = "Yazma", id = 2087}
 	}},
 	{"Freehold", {
-		[1] = {name = "Skycap'n Kragg", id = 2093}, 
-		[2] = {name = "Council o' Captains", id = 2094}, 
-		[3] = {name = "Ring of Booty", id = 2095}, 
+		[1] = {name = "Skycap'n Kragg", id = 2093},
+		[2] = {name = "Council o' Captains", id = 2094},
+		[3] = {name = "Ring of Booty", id = 2095},
 		[4] = {name = "Harlan Sweete", id = 2096}
 	}},
 	{"Kings' Rest", {
-		[1] = {name = "The Golden Serpent", id = 2139}, 
-		[2] = {name = "Mchimba the Embalmer", id = 2142}, 
-		[3] = {name = "The Council of Tribes", id = 2140}, 
+		[1] = {name = "The Golden Serpent", id = 2139},
+		[2] = {name = "Mchimba the Embalmer", id = 2142},
+		[3] = {name = "The Council of Tribes", id = 2140},
 		[4] = {name = "Dazar, The First King", id = 2143}
 	}},
 	{"Shrine of the Storm", {
-		[1] = {name = "Aqu'sirr", id = 2130}, 
-		[2] = {name = "Tidesage Council", id = 2131}, 
-		[3] = {name = "Lord Stormsong", id = 2132}, 
+		[1] = {name = "Aqu'sirr", id = 2130},
+		[2] = {name = "Tidesage Council", id = 2131},
+		[3] = {name = "Lord Stormsong", id = 2132},
 		[4] = {name = "Vol'zith the Whisperer", id = 2133}
 	}},
 	{"Siege of Boralus", {
-		[1] = {name = "Chopper Redhook", id = 2097}, 
-		[2] = {name = "Dread Captain Lockwood", id = 2109}, 
-		[3] = {name = "Hadal Darkfathom", id = 2099}, 
+		--possibly wrong
+		[1] = {name = "Chopper Redhook", id = 2097},
+		[2] = {name = "Dread Captain Lockwood", id = 2109},
+		[3] = {name = "Hadal Darkfathom", id = 2099},
+		--possibly wrong
 		[4] = {name = "Viq'Goth", id = 2100}
 	}},
 	{"Temple of Sethraliss", {
-		[1] = {name = "Adderis and Aspix", id = 2124}, 
-		[2] = {name = "Merektha", id = 2125}, 
-		[3] = {name = "Galvazzt", id = 2126}, 
+		[1] = {name = "Adderis and Aspix", id = 2124},
+		[2] = {name = "Merektha", id = 2125},
+		[3] = {name = "Galvazzt", id = 2126},
 		[4] = {name = "Avatar of Sethraliss", id = 2127}
 	}},
 	{"The MOTHERLODE!!", {
-		[1] = {name = "Coin-Operated Crowd Pummeler", id = 2105}, 
-		[2] = {name = "Azerokk", id = 2106}, 
-		[3] = {name = "Rixxa Fluxflame", id = 2107}, 
+		[1] = {name = "Coin-Operated Crowd Pummeler", id = 2105},
+		[2] = {name = "Azerokk", id = 2106},
+		[3] = {name = "Rixxa Fluxflame", id = 2107},
 		[4] = {name = "Mogul Razdunk", id = 2108},
 	}},
 	{"The Underrot", {
-		[1] = {name = "Elder Leaxa", id = 2111}, 
-		[2] = {name = "Cragmaw the Infested", id = 2118}, 
-		[3] = {name = "Sporecaller Zancha", id = 2112}, 
+		[1] = {name = "Elder Leaxa", id = 2111},
+		[2] = {name = "Cragmaw the Infested", id = 2118},
+		[3] = {name = "Sporecaller Zancha", id = 2112},
 		[4] = {name = "Unbound Abomination", id = 2123}
 	}},
 	{"Tol Dagor", {
-		[1] = {name = "The Sand Queen", id = 2101}, 
-		[2] = {name = "Jes Howlis", id = 2102}, 
-		[3] = {name = "Knight Captain Valyri", id = 2103}, 
+		[1] = {name = "The Sand Queen", id = 2101},
+		[2] = {name = "Jes Howlis", id = 2102},
+		[3] = {name = "Knight Captain Valyri", id = 2103},
 		[4] = {name = "Overseer Korgus", id = 2104}
 	}},
 	{"Waycrest Manor", {
-		[1] = {name = "Heartsbane Triad", id = 2113}, 
-		[2] = {name = "Soulbound Goliath", id = 2114}, 
-		[3] = {name = "Raal the Gluttonous", id = 2115}, 
-		[4] = {name = "Lord and Lady Waycrest", id = 2116}, 
+		[1] = {name = "Heartsbane Triad", id = 2113},
+		[2] = {name = "Soulbound Goliath", id = 2114},
+		[3] = {name = "Raal the Gluttonous", id = 2115},
+		[4] = {name = "Lord and Lady Waycrest", id = 2116},
 		[5] = {name = "Gorak Tul", id = 2117}
 	}}
+)
+
+--[[
+mythicplus = {
+	dungeonname = mapid,
+	mapid = dungeonname,
+	...
+}
+dungeonname and mapid will never clash, so a 2 way table is safe
+]]
+local mythicplus = {}
+(function(...)
+	for _, v in pairs({...}) do
+		mythicplus[v[1]] = v[2]
+		mythicplus[v[2]] = v[1]
+	end
+end)(
+	--TODO
+	{"Atal'Dazar", 0000}
 )
 
 local settings_frame = CreateFrame("Frame", "LTSM_Frame", UIParent, "ButtonFrameTemplate")
@@ -323,6 +345,12 @@ function events:ENCOUNTER_START(id)
 			print(("[LTSM] Settings loot spec to %s."):format(name))
 		end
 	end
+end
+
+--http://www.wowinterface.com/forums/showthread.php?t=54866
+function events:CHALLENGE_MODE_COMPLETED()
+	local map = C_ChallengeMode.GetCompletionInfo()
+	print("[LTSM] Finished key " .. map, "(LOG THIS MAP ID)")
 end
 
 local frame = CreateFrame("Frame")
