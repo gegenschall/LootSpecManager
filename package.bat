254b0d0553429f527ebf93b6583685914a42dab3
Max Liebkies <mail@maxliebkies.de>
2020-11-02 12:42:23 +0100

    Remove old package.bat script

--- package.bat
+++
@@ -1,5 +0,0 @@
-
-rm -rf C:%HOMEPATH%\Desktop\LootSpecManager.zip
-cd ..
-7z a C:%HOMEPATH%\Desktop\LootSpecManager.zip LootSpecManager\core.lua LootSpecManager\data.lua LootSpecManager\gui.lua LootSpecManager\LootSpecManager.toc
-cd LootSpecManager
